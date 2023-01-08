5720c605bcc30791eec5f928ceab98ea2a0a9d8d
madsps <sergey.krulikovskiy@gmail.com>
2023-01-08 17:09:00 +0200

    Button text

--- app/src/static/js/app.js
+++ app/src/static/js/app.js
@@ -106,7 +106,7 @@ function AddItemForm({ onNewItem }) {
                         disabled={!newItem.length}
                         className={submitting ? 'disabled' : ''}
                     >
-                        {submitting ? 'Adding...' : 'Add Item'}
+                        {submitting ? 'Adding...' : 'Add Itemmmmmmmmmmmmmm'}
                     </Button>
                 </InputGroup.Append>
             </InputGroup>
